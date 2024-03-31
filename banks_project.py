a7251ec00fe1a8b7f186e39f45fc65e6deb4b012
purushotham rayi <rayipurushotham98799@gmail.com>
2024-03-31 00:42:38 -0400

    added the skeleton code and the log_progress funtion

---
+++ banks_project.py
@@ -0,0 +1,61 @@
+# Import the required libraries
+
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+import numpy as np 
+import pandas as pd 
+from datetime import datetime
+
+# Define the required variables
+log_file = "code_log.txt"
+target_file = "Largest_banks_data.csv"
+
+# Code for ETL operations on Country-GDP data
+
+
+
+def log_progress(message):
+    ''' This function logs the mentioned message of a given stage of the
+    code execution to a log file. Function returns nothing'''
+
+    timestamp_format = '%Y-%h-%d-%H:%M:%S'
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_format)
+    with open(log_file, 'a') as f:
+        f.write(timestamp + ' : ' + message)
+
+
+def extract(url, table_attribs):
+    ''' This function aims to extract the required
+    information from the website and save it to a data frame. The
+    function returns the data frame for further processing. '''
+
+    return df
+
+def transform(df, csv_path):
+    ''' This function accesses the CSV file for exchange rate
+    information, and adds three columns to the data frame, each
+    containing the transformed version of Market Cap column to
+    respective currencies'''
+
+    return df
+
+def load_to_csv(df, output_path):
+    ''' This function saves the final data frame as a CSV file in
+    the provided path. Function returns nothing.'''
+
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final data frame to a database
+    table with the provided name. Function returns nothing.'''
+
+def run_query(query_statement, sql_connection):
+    ''' This function runs the query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+
+''' Here, you define the required entities and call the relevant
+functions in the correct order to complete the project. Note that this
+portion is not inside any function.'''
+
+
+log_progress('Test message')
